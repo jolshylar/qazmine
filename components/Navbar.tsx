15b7b20319ab3eed4a4f07b8854eaa52f50b904d
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-23 21:05:41 +0600

    feat(auth): next-auth init
    Co-authored-by: Bek Slambek <MugiD@users.noreply.github.com>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const { data: session } = useSession();
   return (
     <nav className="shadow-sm shadow-[#ababab]">
       <ul className="flex text-2xl justify-between px-8 py-4">
@@ -9,12 +11,15 @@ const Navbar = () => {
           <Link href="/">QazMine</Link>
         </li>
         <div className="flex gap-4">
-          <li>
-            <Link href="#siginin">sign in</Link>
-          </li>
-          <li className="text-secondary">
-            <Link href="#login">log in</Link>
-          </li>
+          {session ? (
+            <li className="text-secondary">
+              <button onClick={() => signOut()}>sign out</button>
+            </li>
+          ) : (
+            <li className="text-secondary">
+              <button onClick={() => signIn()}>sign in</button>
+            </li>            
+          )}
         </div>
       </ul>
     </nav>
