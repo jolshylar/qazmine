33cc8e9359b850d9aa5dfd4d5042fe9b5aeec19b
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-23 17:30:07 +0600

    feat(components): navbar style complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="shadow-sm shadow-[#ababab]">
+      <ul className="flex text-2xl justify-between px-8 py-4">
+        <li className="text-secondary">
           <Link href="/">QazMine</Link>
         </li>
-        <div>
+        <div className="flex gap-4">
           <li>
             <Link href="#siginin">sign in</Link>
           </li>
-          <li>
+          <li className="text-secondary">
             <Link href="#login">log in</Link>
           </li>
         </div>
