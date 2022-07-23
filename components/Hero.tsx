811d880b726ff96b5d991c8cdce508ae3bc29228
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-23 21:57:19 +0600

    feat(hero): improve styling
    Co-authored-by: Bek Slambek <MugiD@users.noreply.github.com>

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
   const bgStyles =
     "relative bg-fixed bg-hero-background bg-center bg-cover bg-no-repeat";
   return (
-    <div className={`lg:flex lg:gap-[6%] h-screen ${bgStyles}`}>
+    <div className={`lg:flex lg:gap-[6%] ${bgStyles}`}>
       <div className="uppercase ml-[5%] text-5xl max-w-[760px]">
         <h1 className="pt-[20%]">Collect all elements</h1>
         <h1 className="my-4">Of the periodic table</h1>
-        <h1 className="mb-8">Sitting at home</h1>
+        <h1 className="mb-8 text-secondary">Sitting at home</h1>
         <button className="p-3 rounded-sm bg-teal uppercase text-black">
-          Get Started -&gt;
+          <Link href="#basics">Get Started -&gt;</Link>
         </button>
       </div>
       <Image
         className="lg:bg-chemist lg:bg-cover lg:bg-no-repeat"
         alt="Chemist Portrait"
         src="/images/chemist.png"
-        width={700}
-        height={550}
+        width={600}
+        height={471}
       />
     </div>
   );
