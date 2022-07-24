37f8e74d7c65d613aad03d8c21056cfa7798b233
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-24 19:40:51 +0600

    feat(components): remove navbar from layout for styling
    Co-authored-by: Bek Slambek <MugiD@users.noreply.github.com>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,9 @@
 import React, { ReactNode } from "react";
 import Footer from "./Footer";
-import Navbar from "./Navbar";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
-      <Navbar />
       {children}
       <Footer />
     </div>
