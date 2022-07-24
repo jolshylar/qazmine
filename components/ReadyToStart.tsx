7b5370015ecd47bbf42587722d00e784f3f53dfc
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-24 19:41:11 +0600

    feat(components): design "Ready to start?" section

---
+++ components/ReadyToStart.tsx
@@ -0,0 +1,13 @@
+const ReadyToStart = () => {
+  return (
+    <div className="py-24 text-center p-8 bg-hero-background bg-cover">
+      <h1 className="text-4xl">Ready to start?</h1>
+      <p className="text-xl my-4">Sign in and start rolling!</p>
+      <button className="text-xl border-secondary border-2 rounded-md py-2 px-4">
+        <a href="#hero">Get Started</a>
+      </button>
+    </div>
+  );
+};
+
+export default ReadyToStart;
